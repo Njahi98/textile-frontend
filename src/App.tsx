121612dc0d6824bd9bacc5e16862ce2c743babe1
Njahi98 <njahioussama75@gmail.com>
2025-05-08 23:48:38 +0100

    installed Shadcn and tested adding login UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { GalleryVerticalEnd } from "lucide-react"
+import { LoginForm } from "@/components/login-form"
+
+
 
 function App() {
   return (
-    <div className="flex place-items-center
- text-purple-600">App</div>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10 dark">
+    <div className="flex w-full max-w-sm flex-col gap-6">
+      <a href="#" className="flex items-center gap-2 self-center font-medium">
+        <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+          <GalleryVerticalEnd className="size-4" />
+        </div>
+        Textile Project
+      </a>
+      <LoginForm />
+    </div>
+  </div>
   )
 }
 
