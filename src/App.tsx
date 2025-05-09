acb721ba1eb0f725d08f5027e46a1ae208d97686
Njahi98 <njahioussama75@gmail.com>
2025-05-09 23:44:51 +0100

    implemented login and registration forms with responsive design, started using React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { GalleryVerticalEnd } from "lucide-react"
-import { LoginForm } from "@/components/login-form"
-
-
+import { Route, Routes } from "react-router-dom"
+import { LoginForm } from "@/features/auth/components/Login-form"
+import AuthLayout from "./features/auth/pages/AuthLayout"
+import { RegisterForm } from "./features/auth/components/Register-form"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10 dark">
-    <div className="flex w-full max-w-sm flex-col gap-6">
-      <a href="#" className="flex items-center gap-2 self-center font-medium">
-        <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-          <GalleryVerticalEnd className="size-4" />
-        </div>
-        Textile Project
-      </a>
-      <LoginForm />
-    </div>
-  </div>
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+      <Route path="auth" element={<AuthLayout/>}>
+      <Route path="login" element={ <LoginForm /> } />
+      <Route path="register" element={ <RegisterForm /> } />
+
+      </Route>
+
+
+    </Routes>
+
   )
 }
 
-export default App
+export default App  
